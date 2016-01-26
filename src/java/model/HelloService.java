c4919442e2359b463bdda53f35f8e2a8745bec90
Aly <aschmidt36@my.wctc.edu>
2016-01-25 18:10:23 -0600

    initial commit

---
+++ src/java/model/HelloService.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ *
+ * @author Alyson
+ */
+public class HelloService {
+    public String sayHello(String name){
+        return "Hello " + name + ", isn't MVC great!";
+    }
+}
